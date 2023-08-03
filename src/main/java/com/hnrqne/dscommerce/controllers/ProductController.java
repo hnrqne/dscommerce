3821fdb57f6ac4b866c339ccce3d227e7e25e4be
Henrique Sampaio <hnrqne@gmail.com>
2023-08-02 22:01:42 -0300

    First test with repository

--- src/main/java/com/hnrqne/dscommerce/controllers/ProductController.java
+++ src/main/java/com/hnrqne/dscommerce/controllers/ProductController.java
@@ -1,15 +1,26 @@
 package com.hnrqne.dscommerce.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hnrqne.dscommerce.entities.Product;
+import com.hnrqne.dscommerce.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+	@Autowired
+	private ProductRepository repository;
+	
 	@GetMapping
 	public String teste() {
-		return "teste";
+		Optional<Product> result = repository.findById(1L);
+		Product product = result.get();
+		return product.getName();
 	}
 }
---
+++ src/main/java/com/hnrqne/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.hnrqne.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hnrqne.dscommerce.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
+
