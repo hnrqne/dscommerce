b135772f5058fc979cbee31999474793847c56fd
Henrique Sampaio <hnrqne@gmail.com>
2024-01-18 15:55:58 -0300

    UserDetailsService

---
+++ src/main/java/com/hnrqne/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.hnrqne.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
---
+++ src/main/java/com/hnrqne/dscommerce/services/UserService.java
@@ -0,0 +1,39 @@
+package com.hnrqne.dscommerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.hnrqne.dscommerce.entities.Role;
+import com.hnrqne.dscommerce.entities.User;
+import com.hnrqne.dscommerce.projections.UserDetailsProjection;
+import com.hnrqne.dscommerce.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+		if(result.size() == 0) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		
+		User user = new User();
+		user.setEmail(username);
+		user.setPassword(result.get(0).getPassword());
+		for (UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		
+		return user;
+	}
+}
