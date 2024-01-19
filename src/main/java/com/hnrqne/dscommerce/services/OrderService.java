0e49af8967ab6c989453e663e71adce1797a60e7
Henrique Sampaio <hnrqne@gmail.com>
2024-01-19 16:47:41 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/hnrqne/dscommerce/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.hnrqne.dscommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hnrqne.dscommerce.dto.OrderDTO;
+import com.hnrqne.dscommerce.entities.Order;
+import com.hnrqne.dscommerce.repositories.OrderRepository;
+import com.hnrqne.dscommerce.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public OrderDTO findById(Long id) {
+		
+		Order order = repository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Recurso não encontrado"));
+		return new OrderDTO(order);
+	}
+}
